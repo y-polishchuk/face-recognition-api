9374e7690b143f3e2a809786714a4aaa95a20233
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-03-14 15:19:06 +0200

    add updating entries

--- server.js
+++ server.js
@@ -85,17 +85,11 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    } 
-  })
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(data => res.json(data[0].entries))
+    .catch(err => res.status(400).json('Unable to get entries'))
 })
 
 app.listen(3000, () => {
