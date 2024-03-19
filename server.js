7c2e9fb186ffd3de90215fa415699811ea8aeb7b
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-03-19 14:49:23 +0200

    add express-session

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const session = require('express-session');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const app = express();
 const db = knex({
   client: 'pg',
   connection: {
@@ -13,35 +15,65 @@ const db = knex({
     database : 'smart_brain'
   }
 });
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  optionsSuccessStatus: 200,
+  credentials: true
+}
 
-// db.select('*').from('users').then(data => console.log(data));
-
-const app = express();
-
+app.use(session({
+  secret: 'thisismysecret001',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { 
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    secure: false, // true for production
+    sameSite: 'none'
+  }
+}))
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('success');
 })
 
-app.post('/signin', (req, res) => {
+app.get('/session-status', (req, res) => {
+  if (req.session.user) {
+    res.json({ isSignedIn: true, user: req.session.user });
+  } else {
+    res.json({ isSignedIn: false });
+  }
+});
+
+app.post('/signin', (req, res, next) => {
+  const { email, password } = req.body;
+
   db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
+    .where('email', '=', email)
     .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if(isValid) {
         return db.select('*').from('users')
-          .where('email', '=', req.body.email)
+          .where('email', '=', email)
           .then(user => {
-            res.json(user[0])
+            req.session.regenerate(function (err) {
+              if (err) next(err);
+              req.session.user = user[0];
+              req.session.save(function (err) {
+                if (err) return next(err);
+                res.json(user[0]);
+              })
+            })
           })
-          .catch(err => res.status(400).json('Unable to get user...'))
+          .catch(err => res.status(400).json('Unable to get user...'));
       } else {
         res.status(400).json('Wrong credentials...');
       }
     })
     .catch(err => res.status(400).json('Wrong credentials...'))
+
+  
 })
 
 app.post('/register', (req, res) => {
@@ -62,7 +94,16 @@ app.post('/register', (req, res) => {
           email: data[0].email,
           joined: new Date()
         })
-        .then(user => res.json(user[0]))
+        .then(user => {
+          req.session.regenerate(function (err) {
+            if (err) next(err);
+            req.session.user = user[0];
+            req.session.save(function (err) {
+              if (err) return next(err);
+              res.json(user[0]);
+            })
+          })
+        })
     })
     .then(trx.commit)
     .catch(trx.rollback);
@@ -92,6 +133,17 @@ app.put('/image', (req, res) => {
     .then(data => res.json(data[0].entries))
     .catch(err => res.status(400).json('Unable to get entries'))
 })
+
+app.get('/signout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.status(500).json({ message: 'Failed to sign out' });
+    } else {
+      res.clearCookie('session-id');
+      res.json({ message: 'Signed out successfully' });
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
